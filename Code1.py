dd18c4fd34187546fbd28a41af3fe86913e32a69
unknown <vedantagarwal752@gmail.com>
2026-03-09 06:45:22 +0530

    m

--- Code1.py
+++ Code1.py
@@ -1,21 +1,21 @@
 import pandas as pd
 
-print("hello")
+#print("hello")
 # 1. Read CSV into dataframe
 #df = pd.read_csv("CrowdStrikeEndpoints.csv")
 df = pd.read_excel("CrowdStrikeEndpoints.xlsx")
-# 2. List unique tags
+# 2. Listing unique tags
 unique_tags = df["Tags"].dropna().unique()
 print("Unique Tags:")
 print(unique_tags)
 
-# 3. Sort rows so same tags come together
+# 3. Same Site at one place
 df_sorted = df.sort_values(by="Tags")
 
-# 4. Define MDR sites (tags you want)
+# 4. MDR sites  
 MDR_Sites = {"Hollywood_Grande", "Hollywood_Volume","The_Hollywood_Roosevelt","Malibu_Beach_Inn","Rountree_Consulting","San_Ysidro_Ranch","FalconGroupingTags/San_Ysidro_Ranch","Sunset_Tower_Hotel","Sydell_Miami","The_Core_Club_Fifth"} 
 
-# Filter rows where tag is in MDR_Sites
+# Filtering  MDR_Sites
 mdr_df = df_sorted[df_sorted["Tags"].isin(MDR_Sites)]
 
 # 5. Export to CSV
